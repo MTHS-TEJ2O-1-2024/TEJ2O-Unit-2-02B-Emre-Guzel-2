c631d3e85d0177ecea0275c2c45c2ef45bac4773
Emre-Guzel-2 <yunus.emre.guzel@mths.ca>
2024-09-12 15:56:56 +0000

    changed the names

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,10 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
+Created by: Emre Guzel
+Created on: Sep 12 2020
 This module is a Micro:bit MicroPython program
 """
 
 from microbit import *
 
 
-display.scroll("Hello, World!")
+display.scroll("Hello, Emre Guzel!")
--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Emre Guzel 
+ * Created on: Sep 11 2024
+ * This program say hello Emre Guzel
 */
 
-basic.showString('Hello, World!')
+basic.showString(' Hello ,Emre Guzel ')
